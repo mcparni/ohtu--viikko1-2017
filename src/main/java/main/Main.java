65e50ba9fba5a19e9ab1050991c61c89104b8051
Miika Pärni <miika.parni@helsinki.fi>
2017-11-07 11:30:40 +0200

    codacy test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,10 @@
 package main;
 
-import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Start");
         
     }
 }
